0a7d3f077496f31692e40482674fe56d6f78cf92
delta94 <delta94>
2019-09-23 22:04:12 +0700

    update

--- app.js
+++ app.js
@@ -7,13 +7,23 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
     
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
     return {
         getInput: function() {
             return {
-                type: document.querySelector('.add__type').value, // Will be either inc or exp
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             }
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
         }
     }
 
@@ -23,6 +33,8 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
     
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
 
         // 1. Get the field input data
@@ -34,10 +46,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 4. Calculate the budget
 
-        console.log("It's work");
+
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         if(event.keyCode === 13 || event.which === 13) {
