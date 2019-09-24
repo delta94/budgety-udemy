025728b0488122a3a3d6a84681f986766efe59a6
delta94 <delta94>
2019-09-24 13:38:27 +0700

    Create an initialization function

--- app.js
+++ app.js
@@ -32,8 +32,19 @@ var UIController = (function() {
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-    
-    var DOM = UICtrl.getDOMstrings();
+
+    var setupEventListeners = function() {
+
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            if(event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    }     
 
     var ctrlAddItem = function() {
 
@@ -47,14 +58,14 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
 
+        return {
+            init: function() {
+                setupEventListeners(); 
+        }
+
     }
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        if(event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
-        }
-    });
-
 })(budgetController, UIController)
+
+
+controller.init();
