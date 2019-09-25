de254c6417a3d6ad6f518be2ee52edbf5d324cc5
delta94 <delta94>
2019-09-25 20:44:05 +0700

    add a new item to budget controller

--- app.js
+++ app.js
@@ -24,6 +24,36 @@ var budgetController = (function() {
         }
     }
 
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            if(data.allItems[type].length < 1) {
+                ID = 1;
+            } else {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            }
+            
+            
+            // Create new item based on 'inc' or 'exp' type
+            if(type === 'exp') {
+                newItem = new Expense(ID, des, val)
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val)
+            }
+
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+        },
+        testing: function() {
+            console.log(data);
+        }
+    }
+
 })();
 
 
@@ -75,7 +105,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var input = UICtrl.getInput();
         console.log(input)
         // 2. Add the item to the budget controller
-
+        var newItem = budgetCtrl.addItem(input.type, input.description, input.value)
         // 3. Add the item to the UI
 
         // 4. Calculate the budget
